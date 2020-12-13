d4349d0e27fdf1bb4374422018698b7425ac29f7
Noah Richards <dakeyras@gmail.com>
2020-12-13 15:36:09 -0800

    Day 13, extremely meh.
    This is exactly the type of problem that we're not supposed to ask at
    work anymore, it's a total "gotcha" problem. The brute force solution
    would take days (at best), even if you do cute things like only check
    things in the congruence modulo space for the largest bus id (e.g. my
    answer was 266204454441577 and my largest bus id is 643, so that's a
    lot of checks for the other bus_id congruences).
    
    But if you get the "gotcha" that its the Chinese remainder theorem
    because they are all coprimes(?!) and you remember (?!?!) the algorithm
    for solving it, it's...easy! But that's an obnoxious way to solve
    problems, and this is everything I was hoping advent of code was not.
    
    What a terrible puzzle :(

---
+++ day13/main.cc
@@ -0,0 +1,120 @@
+#include <execution>
+#include <fstream>
+#include <numeric>
+
+#include "absl/container/flat_hash_map.h"
+#include "absl/container/flat_hash_set.h"
+#include "absl/strings/numbers.h"
+#include "absl/strings/str_split.h"
+#include "absl/strings/strip.h"
+#include "absl/strings/substitute.h"
+#include "absl/types/optional.h"
+#include "glog/logging.h"
+
+bool AllPredsTrue(const std::vector<std::function<bool(int64_t)>>& preds,
+                  int64_t number) {
+  // LOG(INFO) << "CHECKING " << number;
+  for (const auto& pred : preds) {
+    if (!pred(number)) return false;
+    LOG(INFO) << "Pred succeeded.";
+  }
+  return true;
+}
+
+// From https://rosettacode.org/wiki/Chinese_remainder_theorem#C.2B.2B
+template <typename _Ty>
+_Ty MulInv(_Ty a, _Ty b) {
+  _Ty b0 = b;
+  _Ty x0 = 0;
+  _Ty x1 = 1;
+
+  if (b == 1) {
+    return 1;
+  }
+
+  while (a > 1) {
+    _Ty q = a / b;
+    _Ty amb = a % b;
+    a = b;
+    b = amb;
+
+    _Ty xqx = x1 - q * x0;
+    x1 = x0;
+    x0 = xqx;
+  }
+
+  if (x1 < 0) {
+    x1 += b0;
+  }
+
+  return x1;
+}
+
+// From https://rosettacode.org/wiki/Chinese_remainder_theorem#C.2B.2B
+template <typename _Ty>
+_Ty ChineseRemainder(std::vector<_Ty> n, std::vector<_Ty> a) {
+  _Ty prod = std::reduce(n.begin(), n.end(), (_Ty)1,
+                         [](_Ty a, _Ty b) { return a * b; });
+
+  _Ty sm = 0;
+  for (int i = 0; i < n.size(); i++) {
+    _Ty p = prod / n[i];
+    sm += a[i] * MulInv(p, n[i]) * p;
+  }
+
+  return sm % prod;
+}
+
+int main(int argc, char** argv) {
+  google::InstallFailureSignalHandler();
+  google::InitGoogleLogging(argv[0]);
+  FLAGS_logtostderr = 1;
+
+  std::ifstream file(argv[1]);
+  CHECK(file);
+
+  std::string line;
+  CHECK(std::getline(file, line));
+  int time_to_depart;
+  CHECK(absl::SimpleAtoi(line, &time_to_depart));
+
+  CHECK(std::getline(file, line));
+  std::vector<int> busses;
+  std::vector<std::string> bus_strings = absl::StrSplit(line, ",");
+  for (auto bus_string : bus_strings) {
+    if (bus_string == "x") continue;
+    int bus;
+    CHECK(absl::SimpleAtoi(bus_string, &bus));
+    busses.push_back(bus);
+  }
+
+  // Part 1: find the bus that leaves closest to but not before our departure
+  // time.
+  int min_distance = INT_MAX;
+  int bus_id = -1;
+  for (int bus : busses) {
+    int distance = bus - (time_to_depart % bus);
+    if (distance < min_distance) {
+      min_distance = distance;
+      bus_id = bus;
+    }
+  }
+  LOG(INFO) << "PART 1: " << bus_id * min_distance;
+
+  // Part 2: lets try some chinese remainder theorem.
+  // Taken from rosettacode.org, because math is hard.
+  std::vector<int64_t> a;
+  std::vector<int64_t> n;
+  for (int64_t i = 0; i < bus_strings.size(); ++i) {
+    int64_t bus_id;
+    if (!absl::SimpleAtoi(bus_strings[i], &bus_id)) continue;
+
+    // Remember that we're looking for next largest, so we're looking for the
+    // coefficient to be bus_id - i.
+    a.push_back(bus_id - i);
+    n.push_back(bus_id);
+  }
+
+  LOG(INFO) << "PART 2: " << ChineseRemainder(n, a);
+  return 0;
+}
